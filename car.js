d7cb3f99534adc307e6f618014d7c4ea15a8c8ff
Shamira Umstead <sumstead@usf.edu>
2024-05-19 21:41:37 -0400

    git commit -m "Add brake method to decrease speed by 5 km/h."

--- car.js
+++ car.js
@@ -4,6 +4,10 @@
 class Car {constructor(make, speed) {this.make = make; this.speed = speed;}}
 
 // Method for accelerating the car by 10 km/h
-accelerate(speedIncrease) {this.speed += 10;
+accelerate() {this.speed += 10;
     console.log(`${this.make} accelerated to ${this.speed} km/h.`);}
+
+// Method for braking the car and decreasing speed by 5 km/h
+brake() {this.speed -= 5;
+    console.log(`${this.make} slowed down to ${this.speed} km/h.`);}
 
