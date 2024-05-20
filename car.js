98b5174138ad7243c50e6d9933eeb02c577da45d
Shamira Umstead <sumstead@usf.edu>
2024-05-19 21:35:13 -0400

    git commit -m "Add accelerate method to increase speed by 10 km/h."

--- car.js
+++ car.js
@@ -1,4 +1,9 @@
 // U78784426
 
 // Car class definition
-class Car {constructor(make, speed) {this.make = make; this.speed = speed;}
+class Car {constructor(make, speed) {this.make = make; this.speed = speed;}}
+
+// Method for accelerating the car by 10 km/h
+accelerate(speedIncrease) {this.speed += 10;
+    console.log(`${this.make} accelerated to ${this.speed} km/h.`);}
+
