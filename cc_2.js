22515b00a3891df37a225b14d9783c9e4d85c852
Shamira Umstead <sumstead@usf.edu>
2024-05-22 11:41:07 -0400

    git commit -m "Added code results to testing."

--- cc_2.js
+++ cc_2.js
@@ -11,7 +11,7 @@ class Car {constructor(make, speed) {this.make = make; this.speed = speed;}}
     // Method used to brake the car and decrease speed by 5 km/h
     Car.prototype.brake = function() {
         this.speed -= 5;
-        console.log(this.make + 'slowed down to' + this.speed + ' km/h.');}
+        console.log(this.make +'slowed down to'+ this.speed +' km/h.');}
 
 // Test Data
 const car1 = new Car('BMW', 120);
@@ -23,14 +23,14 @@ console.log(`${car2.make} initial speed: ${car2.speed} km/h`);
 
 // Testing accelartion and braking methods Car 1
 console.log("Testing Car 1:");
-car1.accelerate();
-car1.brake();
-car1.accelerate();
-car1.brake(); 
+car1.accelerate(); // BMW accelerated to 130 km/h.
+car1.brake(); // BMW slowed down to 125 km/h.
+car1.accelerate(); //BMW accelerated to 135 km/h.
+car1.brake(); // BMW slowed down to 130 km/h.
 
 // Testing accelerating and braking methods Car 2
 console.log("Testing Car 2:");
-car2.accelerate();
-car2.brake();
-car2.accelerate();
-car2.brake();
+car2.accelerate(); // Mercedes accelerated to 105 km/h.
+car2.brake(); // Mercedes slowed down to 100 km/h.
+car2.accelerate(); // Mercedes accelerated to 110 km/h.
+car2.brake(); // Mercedes slowed down to 105 km/h. 
